138fb3fcfd6ab77f46031acfe64fe3f45e873dd0
Pimba Cristovão <pimbacristovao03@gmail.com>
2023-03-25 12:50:27 +0100

    Criada página de recuperação de palavra-passe

---
+++ application/views/layout/cabecalho_recuperar_password.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' ); ?>
+
+<head>
+    <meta charset="utf-8" />
+    <title>SiGE | Sistema de Gestão Escolar | Recuperar palavra-passe</title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="Pimba Cristovão" />
+    <!-- ================== INICIO BASE CSS STYLE ================== -->
+    <link href="<?= base_url(); ?>_assets/plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>_assets/plugins/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>_assets/plugins/font-awesome/5.0/css/fontawesome-all.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>_assets/plugins/ionicons/css/ionicons.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>_assets/plugins/animate/animate.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>_assets/css/apple/style.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>_assets/css/apple/style-responsive.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>_assets/css/apple/croppie.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>_assets/css/apple/theme/default.css" rel="stylesheet" id="theme" />
+	<link href="<?= base_url(); ?>_assets/css/default/style_page_recuperar_password.css" rel="stylesheet">
+    <!-- ================== FIM BASE CSS STYLE ================== -->
+    <script src="<?=base_url(); ?>_assets/plugins/jcrop/js/ie-emulation-modes-warning.js"></script>
+    <link rel="stylesheet" href="<?=base_url(); ?>_assets/plugins/jcrop/css/jquery.Jcrop.css" type="text/css" />
+    <link rel="stylesheet" href="<?=base_url(); ?>_assets/plugins/jcrop/css/ie10-viewport-bug-workaround.css" type="text/css" />
+    <!-- ===================== INICIO BASE JS ==================== -->
+    <script src="<?= base_url(); ?>_assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+    <!-- ====================== FIM BASE JS ===================== -->
+    <!-- INICIO CSS -->
+    <style>
+    .error {
+        color: red;
+    }
+    </style>
+</head>
+
+<body>
+    <!-- INICIO #page-loader -->
+    <!-- <div id="page-loader" class="fade show"><span class="spinner"></span></div>
+    FIM #page-loader -->
