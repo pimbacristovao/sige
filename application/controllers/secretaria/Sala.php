061ec0d20d12cf66f367cfffc18a0c38d17a7b7d
Pimba Cristovão <pimbacristovao03@gmail.com>
2023-05-02 23:53:52 +0100

    Corrigido erro da cláusula where na função listar_turma_coordenacao
    Esse commit adiciona joint para tabela turma_sala

--- application/controllers/secretaria/Sala.php
+++ application/controllers/secretaria/Sala.php
@@ -108,11 +108,11 @@ class Sala extends CI_Controller
 		$dados["sala"] = $this->db->get()->row();										// retorna uma linha
 		/* ================================================================================================== */
 		$this->db->select('*');															// select tudo
-		$this->db->from('turma', 'turma_sala.id_sala');									// da tbl turma
+		$this->db->from('turma', 'turma_sala.id_sala');									// da tbl turma e da tbl turma_sala "id_sala"
 		$this->db->join('classe', 'classe.id_classe = turma.classe_id');		 		// Join tbl classe e turma 
-		$this->db->join('periodo', 'periodo.id_periodo = turma.periodo_id');		 	// Join periodo e turma
-		$this->db->join('turma_sala', 'turma_sala.id_turma = turma.id_turma');
-		$this->db->join('sala', 'sala.id_sala = turma_sala.id_sala');
+		$this->db->join('periodo', 'periodo.id_periodo = turma.periodo_id');		 	// Join tbl periodo e turma
+		$this->db->join('turma_sala', 'turma_sala.id_turma = turma.id_turma');			// Join tbl turma_sala e tbl turma
+		$this->db->join('sala', 'sala.id_sala = turma_sala.id_sala');					// Join tbl sala e turma_sala
 		$this->db->where("turma_sala.id_sala", $id_sala);									// onde id é igual ao id da sala
 		$this->db->order_by("nome_turma", "asc");  										// Ordenar a travez do nome
 		$dados['turmas'] = $this->db->get()->result();									// retorna várias linhas
@@ -142,11 +142,13 @@ class Sala extends CI_Controller
 		$dados["sala"] = $this->db->get()->row();
 		/* ================================================================================================== */
 		$this->db->select('*');															// select tudo
-		$this->db->from('turma');														// da tbl matricula
-		$this->db->where("sala_id", $id_sala);											// onde
-		$this->db->order_by("nome_turma", "asc");  										// Ordenar a travez do nome
-		$this->db->join('classe', 'classe.id_classe = turma.classe_id');		 		// Join turma e matricula
-		$this->db->join('periodo', 'periodo.id_periodo = turma.periodo_id');		 	// Join turma e matricula
+		$this->db->from('turma', 'turma_sala.id_sala');									// da tbl turma e da tbl turma_sala selecione os das id_sala
+		$this->db->join('classe', 'classe.id_classe = turma.classe_id');		 		// Join tbl classe e turma 
+		$this->db->join('periodo', 'periodo.id_periodo = turma.periodo_id');		 	// Join tbl periodo e turma
+		$this->db->join('turma_sala', 'turma_sala.id_turma = turma.id_turma');			// Join tbl turma_sala e turma
+		$this->db->join('sala', 'sala.id_sala = turma_sala.id_sala');					// Join tbl sala e turma_sala
+		$this->db->where("turma_sala.id_sala", $id_sala);									// onde id é igual ao id da sala
+		$this->db->order_by("nome_turma", "asc");										// Ordena através do nome
 		$dados['turmas'] = $this->db->get()->result();									// retorna várias linhas
 		if ( empty($dados["turmas"]) )
 		{
